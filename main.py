a2fc7a9b0a0a5e0944163709c23fa5649fda88ac
PoonamKalita18 <arunakalita2015@gmail.com>
2024-10-16 10:23:44 +0530

    swape

---
+++ main.py
@@ -0,0 +1,11 @@
+x = input("enter value of x ")
+y = input("enter value of y ")
+
+#swapping two numbers
+temp=x
+x=y
+y=temp
+
+#displaying results after swapping
+print("value of x after swapping", x)
+print("value of y after swapping", y)
